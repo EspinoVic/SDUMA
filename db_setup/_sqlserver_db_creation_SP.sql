e4a0fecb597a37cc68dfc83c8fe84a4099bb78fe
EspinoVic <ap.vicespino@gmail.com>
2022-11-08 23:16:11 -0600

    Added SP Expediente creacion

--- db_setup/_sqlserver_db_creation_SP.sql
+++ db_setup/_sqlserver_db_creation_SP.sql
@@ -11,6 +11,8 @@ CREATE PROCEDURE dbo.sp_create_user
     
  AS
     BEGIN TRY
+        DECLARE @rowsInserted INT = @@ROWCOUNT;
+
         BEGIN TRANSACTION CreateUserTran;
             
             SET NOCOUNT ON ;
@@ -31,7 +33,7 @@ CREATE PROCEDURE dbo.sp_create_user
            
 
             DECLARE @personaInsertedIndex int = (SELECT SCOPE_IDENTITY() );
-            DECLARE @rowsInserted INT = @@ROWCOUNT;
+            SET @rowsInserted = @@ROWCOUNT;
 
             INSERT INTO sduma.dbo.[user] (
                 [username],
@@ -110,9 +112,97 @@ INSERT INTO [dbo].[user]
 		   )
 GO
 
- 		
+  		
+CREATE PROCEDURE dbo.sp_create_expediente
+    @nombre nvarchar(255),
+    @apellidoP nvarchar(255),
+    @apellidoM nvarchar(255),
+    @tipoTrámite INT,
+    @idUserCreated INT 
+    
+ AS
+    BEGIN TRY
+        DECLARE @rowsInserted INT = 0;
+		DECLARE @newIdAnual INT;
+
+        BEGIN TRANSACTION CreateExpediente;
+
+            IF NOT (EXISTS ( SELECT  TOP (1) id FROM sduma.dbo.TipoTramite WHERE id = @tipoTrámite))
+            BEGIN ;
+                THROW 54323, 'El tipo de trámite no existe.',1;
+            END;   
 
 
+            DECLARE @currentYear INT = YEAR(GETDATE());
+
+            /* DECLARE @nextIdAnual INT =  */
+            IF NOT EXISTS( SELECT TOP(1) *  FROM dbo.Expediente AS Expe ORDER BY id DESC )
+                BEGIN 
+                    SET  @newIdAnual = 1;
+                END
+            ELSE
+                BEGIN 
+                    SELECT TOP(1) @newIdAnual =
+                        CASE 
+                        -- WHEN ( NOT (EXISTS (anio) ) ) THEN  1/* no hay rows */
+                            WHEN anio = @currentYear THEN  idAnual + 1 
+                            WHEN anio < @currentYear THEN 1 /*  cuando el  */
+                            ELSE /* Exp.anio > @currentYear */ -1
+                        END
+                    FROM dbo.Expediente AS Expe ORDER BY id DESC;
+                END
+
+
+            IF(@newIdAnual = -1)
+            BEGIN ;
+                THROW 54324, 'Incoherencia en fechas de expedientes. -> SP_54323_C_EXPEDIENTE ',1;
+            END;   
+
+
+            INSERT INTO sduma.dbo.Persona ( nombre, apellidoP, apellidoM) 
+            VALUES ( @nombre, @apellidoP, @apellidoP);                               
+
+            DECLARE @personaInsertedIndex int = (SELECT SCOPE_IDENTITY() );
+            SET @rowsInserted = @@ROWCOUNT;
+            
+            INSERT INTO [dbo].[Expediente]
+                    (
+					 [idAnual]
+                    ,[anio]
+                    ,[fechaCreacion]
+                    ,[fechaModificacion]
+                    ,[estado]
+                    ,[id_Persona_Solicita]
+                    ,[id_User_CreadoPor]
+                    ,[id_User_modificadoPor]
+                    ,[id_TipoTramite]
+
+					)
+                VALUES
+                    (@newIdAnual
+                    ,@currentYear
+                    ,GETDATE()
+                    ,GETDATE()
+                    ,1
+                    ,@personaInsertedIndex
+                    ,@idUserCreated 
+                    ,@idUserCreated 
+                    , @tipoTrámite
+                    )
+            SET @rowsInserted = @rowsInserted + @@ROWCOUNT;
+
+            COMMIT TRANSACTION CreateExpediente;
+            SELECT  @rowsInserted AS ROWS_INSERTED;
+    END TRY    
+    BEGIN CATCH
+		 
+        --RAISE  EXCEPTION 
+        ROLLBACK TRANSACTION CreateExpediente;
+        THROW; --@ERROR_NUM, @ERROR_MSG, @ERROR_STATE;
+
+    END CATCH       
+
+GO
 --TEST
 
 /* CREATE PROCEDURE dbo.testSP
