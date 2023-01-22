d5a7220be4f615107de83eeadac2a8c1fc175c73
EspinoVic <ap.vicespino@gmail.com>
2023-01-22 13:19:05 -0600

    Mecanica suelos now wont failes because it was hidden, now is removed from DOM and added

--- frontend/views/site/_niveles.php
+++ frontend/views/site/_niveles.php
@@ -20,28 +20,36 @@ use yii\bootstrap5\Html;
 
 
     <?= $form
-    ->field($mecanicaSuelosFile, '[mecanicaSuelos]myFile',['options' => ['class' => 'col-md-4',"id"=>"inputFilemecanicaSuelos","style"=>"display:none"]])
+    ->field($mecanicaSuelosFile, '[mecanicaSuelos]myFile',['options' => ['class' => 'col-md-4',"id"=>"inputFilemecanicaSuelos",/* "style"=>"display:none" */]])
     ->fileInput([/* 'multiple' => true,  */'accept' => '.pdf,.jpeg,.jpg,.png']) 
     ->label("Mecánica de suelos (subir archivo)")        
     ?>
   
     <script>
         const inputFilemecanicaSuelos = document.getElementById("inputFilemecanicaSuelos");
-
-
+        const inputFileMecanicaSuelosParent = inputFilemecanicaSuelos.parentElement;
         const nivelesChange = function(event){
             //desbloquea FIRMA DRO
             let source = event.target || event.srcElement;
             let nivelesInput = source.value;
-            
+            nivelesHideListener(nivelesInput);
+           
+        }
+
+        const nivelesHideListener = function(nivelesInput){
             if(nivelesInput >= 3){
-                inputFilemecanicaSuelos.style.display = "block";
+                inputFileMecanicaSuelosParent.append(inputFilemecanicaSuelos);
+                //inputFilemecanicaSuelos.style.display = "block";
             }
             else{
-                inputFilemecanicaSuelos.style.display = "none";
-                
+                //inputFilemecanicaSuelos.style.display = "none"; //Problemas al enviar formulario, porque corre la validación aun cuando este hidden.
+                //inputFileMecanicaSuelosParent.removeChild(inputFilemecanicaSuelos);///no usar removeChild (si ya fue removido dará error, no pasa nada, pero mejor evitarlo)
+                //inputFileMecanicaSuelosParent.innerHTML = ""; //tampoco usar, eliminará tambien el field del numero de niveles
+                inputFilemecanicaSuelos.remove();
             }
         }
+        
+        nivelesHideListener(<?= $modelSolicitudGenerica->niveles ?>);        
 
     </script>
 </div>
